05e03247592e976391f95203890e7a18f674cfa2
Chandralekha.s <chandralekha.s@sophos.com>
2020-04-12 13:37:47 +0530

    Dummy message

--- subnet.tf
+++ subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "main" {
   count      = length(data.aws_availability_zones.available.names)
   vpc_id     = aws_vpc.main.id
-  cidr_block = cidrsubnet("10.100.0.0/16",8 ,count.index )
+  cidr_block = cidrsubnet(var.VPC_CIDR,8 ,count.index )
 
   tags = {
     Name = "${var.TAGS["ENV"]}-subnet-${count.index+1}"
