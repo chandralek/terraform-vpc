465ae99fd33dda7a784be7d178d718c6e445e9f7
Chandralekha.s <chandralekha.s@sophos.com>
2020-04-14 05:28:00 +0530

    Dummy message

--- subnet.tf
+++ subnet.tf
@@ -1,7 +1,18 @@
-resource "aws_subnet" "main" {
+resource "aws_subnet" "private-subnets" {
   count      = length(data.aws_availability_zones.available.names)
   vpc_id     = aws_vpc.main.id
   cidr_block = cidrsubnet(var.VPC_CIDR,8 ,count.index )
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.TAGS["ENV"]}-subnet-${count.index+1}"
+  }
+}
+
+resource "aws_subnet" "public-subnets" {
+  count      = length(data.aws_availability_zones.available.names)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = cidrsubnet(var.VPC_CIDR,8 ,count.index+length(data.aws_availability_zones.available.names))
   map_public_ip_on_launch = true
 
   tags = {
