3bac9ef0085dc6101f4879908dc2edc0156e61d2
Chandralekha.s <chandralekha.s@sophos.com>
2020-04-14 19:30:47 +0530

    Dummy message

--- routes.tf
+++ routes.tf
@@ -16,3 +16,22 @@ resource "aws_route_table_association" "public-subnets-association" {
   subnet_id      = element(aws_subnet.public-subnets.*.id, count.index)
   route_table_id = aws_route_table.public-rt.id
 }
+
+resource "aws_route_table" "private-rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.gw.id
+  }
+
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private-subnets-association" {
+  count          = length(data.aws_availability_zones.available.names)
+  subnet_id      = element(aws_subnet.private-subnets.*.id, count.index)
+  route_table_id = aws_route_table.private-rt.id
+}
