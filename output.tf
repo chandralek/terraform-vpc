871960808c3ab437dd202b13df973ae293b99f09
Chandralekha.s <chandralekha.s@sophos.com>
2020-04-14 21:27:20 +0530

    Dummy message

---
+++ output.tf
@@ -0,0 +1,11 @@
+output "VPC_ID" {
+  value = aws_vpc.main.id
+}
+
+output "PUBLIC_SUBNETS" {
+  value = aws_subnet.public-subnets.*.id
+}
+
+output "PRIVATE_SUBNETS" {
+  value = aws_subnet.private-subnets.*.id
+}
