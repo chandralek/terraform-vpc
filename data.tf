b1ee3969960e4a2d8b437744d5ed70b7a3e817c5
Chandralekha.s <chandralekha.s@sophos.com>
2020-04-14 21:12:47 +0530

    Dummy message

--- data.tf
+++ data.tf
@@ -7,7 +7,6 @@ data "aws_vpc" "management" {
   id = var.MGMT_VPC_ID
 }
 
-data "aws_route_tables" "mgmt-route-tables"
-{
+data "aws_route_tables" "mgmt-route-tables" {
   vpc_id = var.MGMT_VPC_ID
 }
